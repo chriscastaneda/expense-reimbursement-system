2c8e812260702f282087b5d7bb3cf2dd555b048b
chris castaneda <chris.j.castaneda@gmail.com>
2020-06-08 16:05:38 -0700

    fixed borken links for express files deleted

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,6 @@ import bodyParser from  'body-parser';
 import { dbConnection } from './daos/db';
 import { userRouter } from './routers/user-router'; 
 import { reimburseRouter } from './routers/reimburse-router'; 
-import { roleRouter } from './routers/role-router'; 
-import { typeRouter } from './routers/type-router';
-import { statusRouter } from './routers/status-router';
-
-
 import { authenticationRouter } from './routers/authenticate-router'; //!NEW LOGIN CODE
 
 /**Initialize */
@@ -21,11 +16,6 @@ app.use(bodyParser.json());
 /**Routers */
 app.use('/users', userRouter);
 app.use('/reimbursements', reimburseRouter);
-app.use('/roles', roleRouter);
-app.use('/types', typeRouter);
-app.use('/status', statusRouter);
-
-
 app.use('/authentication', authenticationRouter) //!NEW LOGIN CODE
 
 
